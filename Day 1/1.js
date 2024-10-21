2b3caaa95361837c728a26bce620fdc72c57c2b6
Manav <gangvanimanav@gmail.com>
2024-10-21 18:37:31 +0530

    DSA

--- Day 1/1.js
+++ Day 1/1.js
@@ -2,10 +2,22 @@
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 let target = 8;
 // let output = 7;
+
+let arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let target1 = 11;
+// let output = -1;
+
 let bag = -1;
 
+// for (let x = 0; x < arr.length; x++) {
+//   if (arr[x] == target) {
+//     bag = x;
+//   }
+// }
+// console.log(bag);
+
 for (let x = 0; x < arr.length; x++) {
-  if (arr[x] == target) {
+  if (arr1[x] == target1) {
     bag = x;
   }
 }
